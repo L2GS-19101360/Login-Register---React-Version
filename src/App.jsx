5b9fde42d2822e32ac495ad37a8e3a50c3b7613f
Lorenz Gil Suico <125742061+L2GS-19101360@users.noreply.github.com>
2023-10-26 02:17:49 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import './App.css'
+import React, { Component } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { BoxArrowRight, PersonPlus } from 'react-bootstrap-icons';
+import reactLogo from './assets/react.svg';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    
+  }
+
+  componentDidMount() {
+
+  }
+  componentWillUnmount() {
+
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <img src={reactLogo} alt="" id='reactlogo' /> &nbsp; Login / Register - React Version
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+
+                </li>
+                <li className="nav-item">
+
+                </li>
+                <li className="nav-item">
+
+                </li>
+              </ul>
+              <form className="d-flex">
+                
+              </form>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom/cjs/react-router-dom.min.js'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
+    <Router>
+      <Route exact path='/' component={App} />
+      
+    </Router>
+  </div>
+)
